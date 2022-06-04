f4461475440eca7c6521c48b2835886ea2c21a37
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-03 21:09:02 -0400

    added alt value to img

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <main className="p-4 bg-pure-white rounded-xl shadow-md max-w-xs">
       <div className="space-y-4 text-center">
-        <img className="rounded-xl" src={qrCode} alt="" />
+        <img className="rounded-xl" src={qrCode} alt="QR Code" />
         <section className="space-y-4 p-4 text-center">
           <h1 className="font-bold text-xl">
             Improve your front-end skills by building projects
