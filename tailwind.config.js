43e2de546d9b14ccec99479d4ffb6218b59180e5
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-03 20:51:53 -0400

    extended colors and fontFamily values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pure-white': 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
